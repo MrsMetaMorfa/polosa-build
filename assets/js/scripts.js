e492721bde53706f358b11eb5a28cad6dd9f3986
MrsMetaMorfa <maitressse.metamorfa@gmail.com>
2020-05-08 22:08:23 +0500

    Bugfix: iteration 1

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,6 +9,7 @@
 // Аналог $(document).ready()
 document.addEventListener('DOMContentLoaded', function() {
   function StartScripts() {
+
     if (document.querySelector('.compilation')) {
       let itemsOnPage = document.querySelectorAll('.item'), itemWidth, imageHeight;
       if ((window.innerWidth < 1024) && (window.innerWidth < window.innerHeight)) {
@@ -29,18 +30,25 @@ document.addEventListener('DOMContentLoaded', function() {
         });
       }
     }
+
     if (document.querySelector('.article')) {
       let articleImage = document.querySelector('.article_image'),
           imageHeight, imageWidth;
       if ( (window.innerWidth < 1024) && (window.innerWidth < window.innerHeight) ) {
+        articleImage.style.width = 'inherit';
+        articleImage.style.height = 'inherit';
         imageWidth = articleImage.offsetWidth;
-        imageHeight = (imageWidth) / 141 * 212;
+        imageHeight = articleImage.offsetHeight;
         console.log(imageHeight, imageWidth);
+        articleImage.nextElementSibling.style.left = 0;
         articleImage.style.height = imageHeight + 'px';
         articleImage.nextElementSibling.querySelector('.header').style.marginBottom = imageHeight + 'px';
       } else {
+        articleImage.style.width = 'inherit';
+        articleImage.style.height = 'inherit';
         imageHeight = articleImage.offsetHeight;
         imageWidth = (imageHeight) / 212 * 141;
+        articleImage.nextElementSibling.querySelector('.header').style.marginBottom = 'inherit';
         articleImage.style.width = imageWidth + 'px';
         articleImage.nextElementSibling.style.left = imageWidth + 'px';
       }
